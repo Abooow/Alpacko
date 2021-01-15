c4e2115aa4b3608030d4617f96e006a49d84ec89
raiteiryuu <kitt.cheung01@gmail.com>
2021-01-15 11:17:14 +0100

    sign up page fix

--- src/Alpacko.Client.Web/Models/SignUpModel.cs
+++ src/Alpacko.Client.Web/Models/SignUpModel.cs
@@ -8,20 +8,32 @@ namespace Alpacko.Client.Web.Models
 {
     public class SignUpModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage = "First name is required")]
+        [MaxLength(26, ErrorMessage = "First name is too long")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [MaxLength(26, ErrorMessage = "Last name is too long")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "E-mail is required")]
+        [MaxLength(254, ErrorMessage = "E-mail is too long")]
+        [EmailAddress(ErrorMessage = "Not a valid e-mail address")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Password is required")]
+        [MaxLength(128, ErrorMessage = "Password is too long")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must be at least 8 characters, and include at least one lowercase letter, one uppercase letter, and a number")]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Please re-type your password")]
+        [Compare(nameof(Password), ErrorMessage = "Password does not match")]
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Please indicate that you have read and agree to the Terms of Use")]
+        public bool AgreeToTerms { get; set; }
     }
 }
